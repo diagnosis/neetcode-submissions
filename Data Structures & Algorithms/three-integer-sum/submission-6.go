b25c9c81567b1ecddbb47a576cfad2e4eed3daaa
safa demirkan <demirkansafa@gmail.com>
2026-06-27 10:26:52 -0700

    Add: three-integer-sum - submission-6

---
+++ Data Structures & Algorithms/three-integer-sum/submission-6.go
@@ -0,0 +1,32 @@
+
+import "slices"
+func threeSum(nums []int) [][]int {
+	 var res [][]int
+	 slices.Sort(nums)
+	 for i := 0; i < len(nums) ; i ++ {
+		target := -nums[i]
+		left := i + 1
+		right := len(nums) -1
+		if i >0 && nums[i] == nums[i-1]{
+			continue
+		}
+		for left < right {
+			if nums[left] + nums[right] > target{
+				right--
+			}else if nums[left] + nums[right] < target{
+				left++
+			}else{
+				res = append(res, []int{nums[left], nums[right], -target})
+				left++
+				right--
+				for left < right && nums[left] == nums[left-1]{
+					left++
+				}
+				for left < right && nums[right] == nums[right+1]{
+					right--
+				}
+			}
+		}
+	 }
+	 return res
+}
