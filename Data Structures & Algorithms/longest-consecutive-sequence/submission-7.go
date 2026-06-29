8972366e03db93313f1d41efa0ca23bad505a537
safa demirkan <demirkansafa@gmail.com>
2026-06-29 11:59:39 -0700

    Add: longest-consecutive-sequence - submission-7

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-7.go
@@ -0,0 +1,27 @@
+func longestConsecutive(nums []int) int {
+	set := make(map[int]struct{})
+	for _, num := range nums{
+		set[num]= struct{}{}
+	}
+
+	maxV := 0
+
+	for i := 0 ; i < len(nums); i++ {
+		
+		if _, ok := set[nums[i]-1]; !ok {
+			count := 1
+			start := nums[i]+1
+			for{
+				if _, ok := set[start]; !ok{
+					break
+				}
+				start++
+				count++
+			}
+			maxV = max(maxV, count)
+		}
+
+	}
+	return maxV
+
+}
