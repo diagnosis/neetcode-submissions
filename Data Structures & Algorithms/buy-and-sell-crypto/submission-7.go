f897bb4bf568707faf51d7650ad56dca0f4a10a4
safa demirkan <demirkansafa@gmail.com>
2026-06-29 19:27:45 -0700

    Add: buy-and-sell-crypto - submission-7

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-7.go
@@ -0,0 +1,12 @@
+func maxProfit(prices []int) int {
+	minPrice := prices[0]
+	profit := 0
+	
+	for i :=1 ; i < len(prices); i++{
+		if prices[i] < minPrice{
+			minPrice = prices[i]
+		}
+		profit = max(profit, prices[i]-minPrice)
+	}
+	return profit
+}
