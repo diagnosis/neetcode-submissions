07807a6fadb4046f4ee887c3b9692b49ddab1731
safa demirkan <demirkansafa@gmail.com>
2026-07-10 22:05:03 -0700

    Add: binary-search - submission-4

---
+++ Data Structures & Algorithms/binary-search/submission-4.go
@@ -0,0 +1,15 @@
+func search(nums []int, target int) int {
+	left := 0
+	right := len(nums) - 1
+	for left <= right {
+		med := left + (right-left)
+		if nums[med] == target {
+			return med
+		} else if nums[med] > target {
+			right = med - 1
+		} else {
+			left = med + 1
+		}
+	}
+	return -1
+}
